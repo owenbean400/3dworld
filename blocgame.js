d3713a7d2206ea87ad90b40ecfc352469e40474e
owenbean400 <owenbean400@gmail.com>
2020-10-21 16:02:25 -0400

    add ball with moving it

--- blocgame.js
+++ blocgame.js
@@ -3,34 +3,31 @@ import {PointerLockControls} from 'https://cdn.jsdelivr.net/npm/three@0.121.1/ex
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js';
 
 var camera, scene, renderer, controls;
-
-var objects = [];
-
 var raycaster;
 
 var basketball;
+var basketballHeld = false;
+var basketballPrevCords = [];
+var previousCameraGrabBall = [];
 
-var dribble = false;
-var dribbleDown = false;
-var ballShot = false;
-
-var shootReady = false;
-
-var ballVelocity = 1;
-var ballVelocityZ, ballVelocityY;
+var cameraPrevQuat;
 
 var collidableMeshList = [];
+var basketballCollision = [];
+
+var raycasterMouse = new THREE.Raycaster();
+var mouseVector = new THREE.Vector2();
+var lookAtBall = false;
+var grabBall = false;
 
 var cube2;
+var distanceBallCamera;
 
 var moveForward = false;
 var moveBackward = false;
 var moveLeft = false;
 var moveRight = false;
 var canJump = false;
-
-var scope = false;
-var shot = false;
 
 var prevTime = performance.now();
 var velocity = new THREE.Vector3();
@@ -45,13 +42,12 @@ function init() {
 
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
     camera.position.y = 10;
-    console.log(camera);
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0x999999 );
     scene.fog = new THREE.Fog( 0x999999, 0, 750 );
 
-    var light = new THREE.AmbientLight( 0x404040, 1 ); // soft white light
+    var light = new THREE.AmbientLight( 0x404040, 2 ); // soft white light
     scene.add( light );
 
     //light
@@ -75,7 +71,6 @@ function init() {
     directionalLight.shadow.camera.top = d;
     directionalLight.shadow.camera.bottom = - d;
 
-    console.log(directionalLight);
     var helper = new THREE.CameraHelper( directionalLight.shadow.camera );
     scene.add( helper );
     scene.add( directionalLight );  
@@ -89,7 +84,7 @@ function init() {
     instructions.addEventListener( 'click', function () {
 
         controls.lock();
-        document.getElementById('cursor').style.display = "none";
+        crosshair.style.display = "none";
 
 
     }, false );
@@ -98,7 +93,7 @@ function init() {
 
         instructions.style.display = 'none';
         blocker.style.display = 'none';
-        document.getElementById('cursor').style.display = '';
+        crosshair.style.display = '';
 
     } );
 
@@ -106,7 +101,7 @@ function init() {
 
         blocker.style.display = 'block';
         instructions.style.display = '';
-        document.getElementById('cursor').style.display = "none";
+        crosshair.style.display = "none";
 
     } );
 
@@ -182,51 +177,53 @@ function init() {
     document.addEventListener( 'keyup', onKeyUp, false );
     
     var onMouseDown = function (event){
-        if(event.button == 2){
-            shootReady = true;
+        if(event.button == 2 && lookAtBall){
+            grabBall = true;
+            cameraPrevQuat = camera.quaternion.clone();
+            basketballPrevCords.push(basketball.position.x);
+            basketballPrevCords.push(basketball.position.y);
+            basketballPrevCords.push(basketball.position.z);
+            previousCameraGrabBall.push(camera.position.x);
+            previousCameraGrabBall.push(camera.position.y);
+            previousCameraGrabBall.push(camera.position.z);
+            previousCameraGrabBall.push(camera.getWorldDirection());
+            basketballHeld = true;
+            distanceBallCamera = threeDistance(camera.position.x, camera.position.y, camera.position.z, basketball.position.x, basketball.position.y, basketball.position.z);
+            scene.remove(basketball);
+            basketball.position.set(0, 0, -1 * distanceBallCamera);
+            camera.add(basketball);
         }
     }
 
     var onMouseUp = function (event) {
-        if(event.button == 2){
-            shootReady = false;
+        if(event.button == 2 && lookAtBall){
+            var RotationDisplacementX = distanceBallCamera * (camera.getWorldDirection().x - previousCameraGrabBall[3].x);
+            var RotationDisplacementY = distanceBallCamera * (camera.getWorldDirection().y - previousCameraGrabBall[3].y);
+            var RotationDisplacementZ = distanceBallCamera * (camera.getWorldDirection().z - previousCameraGrabBall[3].z);
+            basketballHeld = false;
+            basketball.position.set(RotationDisplacementX + basketballPrevCords[0] + camera.position.x - previousCameraGrabBall[0], RotationDisplacementY + basketballPrevCords[1] + camera.position.y - previousCameraGrabBall[1], RotationDisplacementZ + basketballPrevCords[2] + camera.position.z - previousCameraGrabBall[2],)
+            camera.remove(basketball);
+            scene.add(basketball);
+            grabBall = false;
+            basketballPrevCords = [];
+            previousCameraGrabBall = [];
         }
     }
 
-    var mouseClick = function (event) {
-        if(event.button == 0){
-            if(!dribble){
-                dribbleDown = true;
-                dribble = true;
-            }
-        }
+    var mouseMove = function (event) {
+        event.preventDefault();
+        mouseVector.x = (event.clientX / window.innerWidth ) * 2 - 1;
+        mouseVector.y = - ( event.clientY / window.innerHeight ) * 2 + 0.8;
+        console.log(event.clientY);
+        console.log(mouseVector);
     }
 
     document.addEventListener( 'mousedown', onMouseDown, false);
     document.addEventListener( 'mouseup', onMouseUp, false);
-    document.addEventListener( 'click', mouseClick, false);
+    document.addEventListener( 'mousemove', mouseMove, false);
 
     raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
 
-    /*
-    var planeGeometry = new THREE.PlaneBufferGeometry( 200, 200, 32, 32 );
-    var planeMaterial = new THREE.MeshStandardMaterial( { color: 0x00ff00 } )
-    var plane = new THREE.Mesh( planeGeometry, planeMaterial );
-    plane.rotation.x = -1 * Math.PI / 2;
-    plane.receiveShadow = true;
-    scene.add( plane );
-    */
-
-    // objects
-    /*
-    var geo = new THREE.BoxGeometry(10,30,10, 10, 10, 10);
-    var mat = new THREE.MeshStandardMaterial({color: 0xff0000});
-    var cube = new THREE.Mesh( geo, mat);
-    cube.position.set(-10, 0, -10);
-    cube.castShadow = true;
-    scene.add(cube);
-    collidableMeshList.push(cube);
-    */
     var geo2 = new THREE.CylinderGeometry(5, 5, 8, 12);
     var mat2 = new THREE.MeshStandardMaterial({color: 0xff0000});
     cube2 = new THREE.Mesh( geo2, mat2);
@@ -234,16 +231,6 @@ function init() {
     cube2.scale.set (1, 2, 1);
     cube2.castShadow = true;
     camera.add(cube2);
-
-    /*
-    var geometryq = new THREE.ConeGeometry( 5, 3, 6 );
-    var materialq = new THREE.MeshStandardMaterial( {color: 0xffff00} );
-    var sphere = new THREE.Mesh( geometryq, materialq );
-    sphere.position.set(20, 0, 20);
-    sphere.castShadow = true;
-    scene.add( sphere );
-    collidableMeshList.push(sphere);
-    */
 
     var loader = new GLTFLoader();
 
@@ -255,10 +242,12 @@ loader.load( './court.glb', function ( gltf ) {
     model.position.set(0, -2, 0);
     model.castShadow = true;
     model.receiveShadow = true;
-    console.log(model);
     for(let i = 0; i < model.children.length; i++){
         model.children[i].castShadow = true;
+        model.children[i].receiveShadow = true;
         collidableMeshList.push(model.children[i]);
+        if(model.children[i].type == "Mesh")
+        basketballCollision.push(model.children[i]);
     }
     scene.add( model );
 
@@ -267,6 +256,19 @@ loader.load( './court.glb', function ( gltf ) {
 	console.error( error );
 
 } );
+    var geos = new THREE.SphereGeometry( 5, 12, 12 );
+    var mats = new THREE.MeshStandardMaterial( {color: 0xffa500} );
+    basketball = new THREE.Mesh( geos, mats );
+    var wireframe = new THREE.WireframeGeometry( geos );
+    var sline = new THREE.LineSegments( wireframe );
+    sline.material.depthTest = false;
+    sline.material.opacity = 0.02;
+    sline.material.transparent = true;
+    basketball.position.set(5, 6, -20);
+    basketball.castShadow = true;
+    basketball.receiveShadow = true;
+    scene.add( basketball );
+    basketball.add( sline );
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
     renderer.setPixelRatio( window.devicePixelRatio );
@@ -274,6 +276,8 @@ loader.load( './court.glb', function ( gltf ) {
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     document.body.appendChild( renderer.domElement );
+    document.getElementsByTagName("canvas")[0].style.top = 0;
+    document.getElementsByTagName("canvas")[0].style.left = 0;
 
     //
 
@@ -297,8 +301,7 @@ function animate() {
 
     requestAnimationFrame( animate );
 
-    var time = performance.now();	 
-
+    var time = performance.now();
 
     if ( controls.isLocked === true ) {
 
@@ -340,10 +343,8 @@ function animate() {
             var collisionResults = ray.intersectObjects( collidableMeshList );
             if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ) {
                 var errorMargin = 0.5;
-                    console.log(collisionResults);
                 if(collisionResults[0].face.normal.y > Math.abs(collisionResults[0].face.normal.x) && collisionResults[0].face.normal.y > Math.abs(collisionResults[0].face.normal.z)){
                     camera.position.y = prevCameraInfoPositionY;
-                    console.log("y")
                     canJump = true;
                     velocity.y = Math.max(0, velocity.y);
                     if(Math.abs(collisionResults[0].face.normal.x) == Math.max(Math.abs(collisionResults[0].face.normal.x),Math.abs(collisionResults[0].face.normal.z) + errorMargin * 2)){
@@ -360,34 +361,39 @@ function animate() {
                     }
                 }
                 else if(Math.abs(collisionResults[0].face.normal.x) == Math.max(Math.abs(collisionResults[0].face.normal.x),Math.abs(collisionResults[0].face.normal.z) + errorMargin)){
-                    console.log("x");
                     camera.position.x = prevCameraInfoPositionX;
                     camera.position.z = prevCameraInfoPositionZ;
                 }
                 else if(Math.abs(collisionResults[0].face.normal.z) == Math.max(Math.abs(collisionResults[0].face.normal.x) + errorMargin,Math.abs(collisionResults[0].face.normal.z))){
-                    console.log("z");
                     camera.position.z = prevCameraInfoPositionZ;
                 }
                 else{
-                    console.log("else");
                     camera.position.x = prevCameraInfoPositionX;
                     camera.position.z = prevCameraInfoPositionZ;
                 }
             }
         }
-
-        
-        //console.log(camera.position);
-
     }
     prevTime = time;
-
-    
-
     renderer.render( scene, camera );
 
-    
-    //console.log("old X:" + prevCameraInfoPositionX + "   new X:"+ camera.position.x);
+    raycasterMouse.setFromCamera( mouseVector, camera);
+
+    var intersects = raycasterMouse.intersectObjects( basketball.children );
+    if(intersects.length > 0 || grabBall){
+        lookAtBall = true;
+        basketball.material.color.set( 0xffff00);
+    }
+    else{
+        lookAtBall = false;
+        basketball.material.color.set( 0xffa500);
+    }
+}
+
+
+function threeDistance(x1, y1, z1, x2, y2, z2){
+    var distance = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2) + Math.pow((z2 - z1),2));
+    return distance;
 }
 
 
